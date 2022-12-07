d5a5c317171008df4afd8a5264fe463859045397
MadDinosaur <1191507@isep.ipp.pt>
2022-12-07 14:17:07 +0100

    changes you made

--- VRTK-master/Assets/Basketball Court/Scripts/ScorePoint.cs
+++ VRTK-master/Assets/Basketball Court/Scripts/ScorePoint.cs
@@ -13,11 +13,23 @@ public class ScorePoint : MonoBehaviour
     public TMP_Text timeBox;
     public Animator peopleAnimator;
     System.Random rnd = new System.Random();
+
+    public List<Animator> allCharacters = new List<Animator>();
     
     // Start is called before the first frame update
     void Start()
     {
         points = 0;
+
+        foreach (Animator item in FindObjectsOfType<Animator>())
+        {
+            if (item.runtimeAnimatorController.name == "People Controller")
+            {
+                allCharacters.Add(item);
+            }
+        }
+        
+
     }
 
     // Update is called once per frame
@@ -38,8 +50,18 @@ public class ScorePoint : MonoBehaviour
             points += 1;
             confetti.Play();
             textBox.SetText(points.ToString());
-            peopleAnimator.SetInteger("AnimationNum", rnd.Next(1, 4));
-            peopleAnimator.SetInteger("AnimationNum", 0);
+
+            foreach (var character in allCharacters)
+            {
+                AssignAnimation(character);
+            }
+
         }
+    }
+
+    public void AssignAnimation(Animator cc)
+    {
+        cc.SetInteger("AnimationNum", rnd.Next(1, 4));
+        cc.SetTrigger("Cheer");
     }
 }
