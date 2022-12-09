92e1a6d5fb25eb037fa0800fb31c77b46e06751d
MadDinosaur <1191507@isep.ipp.pt>
2022-12-09 16:55:32 +0100

    Changes

---
+++ VRTK-master/Assets/UI/PopUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+
+    public GameObject uiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Headset")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+
+}
