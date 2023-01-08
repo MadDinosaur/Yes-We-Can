49dd0160d049053737610793c7d0e62998164f1f
loganpetet <loganpetetgmail.com>
2023-01-08 12:06:51 +0100

    audio

---
+++ VRTK-master/Assets/UI/Scripts/SpatialPopUpVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialPopUpVideo : MonoBehaviour
+{
+
+    public GameObject uiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Headset")
+        {
+            //Deactivate collider
+            GetComponent<Collider>().enabled = false;
+            //Activate UI overlay
+            uiObject.SetActive(true);
+            //Determine duration of voice line and play
+            UnityEngine.Video.VideoPlayer voiceLine = transform.parent.gameObject.GetComponentInChildren<UnityEngine.Video.VideoPlayer>();
+            double duration = voiceLine.clip.length;
+            //Keep UI active until voice line is done
+            StartCoroutine(WaitForSec((float)duration));
+        }
+    }
+    IEnumerator WaitForSec(float seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+
+        Destroy(uiObject);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ VRTK-master/Assets/UI/Scripts/TimePopUpVideo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePopUpVideo : MonoBehaviour
+{
+    public GameObject uiObject;
+    public float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+        StartCoroutine(WaitForTimeTrigger(duration));
+    }
+
+    IEnumerator WaitForVoiceLine(float seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+
+        Destroy(uiObject);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator WaitForTimeTrigger(float seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+
+        //Activate UI overlay
+        uiObject.SetActive(true);
+        //Determine duration of voice line and play
+        UnityEngine.Video.VideoPlayer voiceLine = GetComponentInChildren<UnityEngine.Video.VideoPlayer>();
+        double clipLength = voiceLine.clip.length;
+        //Keep UI active until voice line is done
+        StartCoroutine(WaitForVoiceLine((float)clipLength));
+    }
+}
